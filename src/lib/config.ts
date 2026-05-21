0122e43dc0f784ba88269e24014d044fabe5ec2b
hoon.hong <hoon.hong@dktechin.com>
2026-05-21 17:32:59 +0900

    .
    .

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,14 +2,14 @@
  * Meta Instagram API 설정
  * 나중에 실제 값으로 교체하세요.
  */
-export const INSTAGRAM_USER_ID = "YOUR_INSTAGRAM_USER_ID";
-export const INSTAGRAM_ACCESS_TOKEN = "YOUR_INSTAGRAM_ACCESS_TOKEN";
+export const INSTAGRAM_USER_ID = "1446745077479587";
+export const INSTAGRAM_ACCESS_TOKEN = "IGAAUjzpZAseKNBZAFpJeTVqSzgwM0djVUhNTGpFS1l5dVlWcWU3TUlkOTFzZADMtYTRfSXpleFl6bzhZAcURwMkdfbGZAHNGxGTW5aRXhld3UtdnYtTWt3Wk41dk1qNzA5Y0ZANbU5ZAMWNBd192aFliRnA5a0d0Y0lVMGQzRHMtSUVBMAZDZD";
 
 /** Meta App Dashboard > Webhooks > Verify Token 과 동일해야 함 */
-export const WEBHOOK_VERIFY_TOKEN = "YOUR_WEBHOOK_VERIFY_TOKEN";
+export const WEBHOOK_VERIFY_TOKEN = "instagram-dm-verify-2026";
 
 /** App Dashboard > Settings > Basic > App Secret (웹훅 서명 검증용) */
-export const META_APP_SECRET = "YOUR_META_APP_SECRET";
+export const META_APP_SECRET = "539a63e05e41247a0cddfd20d3fb7e8d";
 
 /** graph.facebook.com (Facebook Login) | graph.instagram.com (Instagram Login) */
 export const GRAPH_API_HOST = "graph.facebook.com";
